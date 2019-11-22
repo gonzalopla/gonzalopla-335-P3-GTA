083af1333d3fb94fc007ffcc8170f53090448574
gonzalopla <gonzalo_p@csu.fullerton.edu>
2019-11-21 16:37:00 -0800

    Add files via upload

---
+++ algo.js
@@ -0,0 +1,119 @@
+//merge
+let selectionSort = (arr) => {
+
+    //Loop till the second last element
+    for(let i = 0; i < arr.length - 1; i++){
+ 
+       //Loop after the i till the last element
+       for(let j = i + 1; j < arr.length; j++){
+ 
+          //if jth element is less than the ith element then swap
+          //change < to > for sorting in descending order
+          if(arr[j] < arr[i]){
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+          }
+ 
+       }
+ 
+    }
+    
+    //return the sorted array
+    return arr;
+ }
+ if(arr[j] < arr[i]){
+    [ arr[i], arr[j] ] = [ arr[j], arr[i] ];
+  }
+
+
+
+
+  // Merge Sort Implentation (Recursion)
+function mergeSort (unsortedArray) {
+    // No need to sort the array if the array only has one element or empty
+    if (unsortedArray.length <= 1) {
+      return unsortedArray;
+    }
+    // In order to divide the array in half, we need to figure out the middle
+    const middle = Math.floor(unsortedArray.length / 2);
+  
+    // This is where we will be dividing the array into left and right
+    const left = unsortedArray.slice(0, middle);
+    const right = unsortedArray.slice(middle);
+  
+    // Using recursion to combine the left and right
+    return merge(
+      mergeSort(left), mergeSort(right)
+    );
+  }
+  // Merge the two arrays: left and right
+  function merge (left, right) {
+    let resultArray = [], leftIndex = 0, rightIndex = 0;
+  
+    // We will concatenate values into the resultArray in order
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        resultArray.push(left[leftIndex]);
+        leftIndex++; // move left array cursor
+      } else {
+        resultArray.push(right[rightIndex]);
+        rightIndex++; // move right array cursor
+      }
+    }
+  
+    // We need to concat here because there will be one element remaining
+    // from either left OR the right
+    return resultArray
+            .concat(left.slice(leftIndex))
+            .concat(right.slice(rightIndex));
+  }
+  if (unsortedArray.length <= 1) { 
+      return unsortedArray; 
+  }
+
+
+
+  // quick
+  var items = [5,3,7,6,2,9];
+function swap(items, leftIndex, rightIndex){
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+// first call to quick sort
+var sortedArray = quickSort(items, 0, items.length - 1);
+console.log(sortedArray); //prints [2,3,5,6,7,9]
